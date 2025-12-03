1bbd936af0e4bf9eb37c7c2d474adfd9e207a8ab
markd <markd@ekconsulting.net>
2025-12-03 17:34:15 +0800

    Has Status

--- src/Models/PaymentAccount.php
+++ src/Models/PaymentAccount.php
@@ -1,10 +1,12 @@
 <?php
 namespace Mdayo\Payment\Models;
 use Illuminate\Database\Eloquent\Model;
-
+use Mdayo\Payment\Models\Traits\HasStatus;
 
 class PaymentAccount extends Model
 {
+    use HasStatus;
+    
     protected $guarded = [];
     public function owner()
     {
@@ -14,4 +16,5 @@ class PaymentAccount extends Model
     {
         $this->belongsTo(PaymentProvider::class,'payment_provider_id');
     }
+   
 }
--- src/Models/Traits/HasPaymentAccount.php
+++ src/Models/Traits/HasPaymentAccount.php
@@ -1,6 +1,7 @@
 <?php
 namespace Mdayo\Payment\Models\Traits;
 use Mdayo\Payment\Models\PaymentAccount;
+use Mdayo\Payment\Models\PaymentProvider;
 
 trait HasPaymentAccount
 {
@@ -12,5 +13,13 @@ trait HasPaymentAccount
     public function paymentAccounts()
     {
         return $this->morphMany(PaymentAccount::class,'owner');
+    }
+    public function createPaymentAccount(PaymentProvider $payment_provider,string $account_number, string $account_holder_name)
+    {
+        return $this->paymentAccount()->firstOrCreate([
+            'payment_provider_id'=>$payment_provider->id,
+            'payment_provider_type'=>$payment_provider::class,
+            'account_number'=>$account_number,
+        ],['account_holder_name'=> $account_holder_name]);
     }
 }
